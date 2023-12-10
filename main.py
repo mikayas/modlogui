52024f92bf9ee3055410be76e9cf3eef208c9bf1
mikayas <86917713+mikayas@users.noreply.github.com>
2023-12-10 18:22:28 -0300

    feat(main.py): added function (main)

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import flet as ft
+from routes import routes
+from head import head
+
+
+def main(page: ft.Page):
+    head(page=page)
+    page.on_route_change = routes(page=page, route=any)
+    page.go(page.route)
+
+
+ft.app(target=main)
 
